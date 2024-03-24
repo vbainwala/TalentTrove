0bcae10db0c08e85bb312c8844d57a646b60c442
JTL-lab <juliatlewand@gmail.com>
2024-03-24 14:54:05 -0400

    Resolves #15: add multi-select option to filter jobs by type of job (full time, internship, and/or coop)

--- web_app/server.py
+++ web_app/server.py
@@ -105,55 +105,82 @@ def login():
 
 @app.route('/job_board',methods=('GET','POST'))
 def job_board():
-	if request.method=="POST":
+	if request.method == "POST":
+
+		# Collect all user selected field values
 		location = request.form['Location']
 		company = request.form['Company']
 		skills = request.form['Skills']
 		search = request.form['Search']
 		experience = request.form['Experience']
-		print(location,company)
-		if not search:
-			filter_query = """SELECT j.Job_ID,j.Job_Title, j.Experience, j.Location, j.Requirements, j.Skills, c.Name as Company_Name, r.Name as Recruiter_Name
-							FROM Job_Posting j
-							JOIN Company c ON j.Company_ID = c.Company_ID
-							JOIN Recruiter r ON j.Recruiter_Username = r.Username
-							WHERE j.location LIKE :location 
-							AND c.Name LIKE :company
-							AND j.Skills LIKE :skills
-							AND j.Experience LIKE :experience"""
-			params = {}
-			params["location"]= "%"+location+"%"
-			params["company"] = "%"+company+"%"
-			params["skills"] = "%"+skills+"%"
-			params["experience"] = "%"+experience+"%"
-			# print(params)
-			filter_query = text(filter_query)
-			cursor = g.conn.execute(filter_query,params)
-			postings = cursor.fetchall()
-			# print(postings)
-			cursor.close()
-			return render_template('job_board.html',postings=postings)
+		role_types = request.form['role_types']
+
+		# Keep track of all user selected fields to filter by to build SQL query
+		where_clauses = []
+		params = {}
+
+		# Default query (i.e. what is executed if user does not select anything specific to filter by)
+		base_search_query = """SELECT j.Job_ID, j.Job_Title, j.Experience, j.Location, j.Requirements, j.Skills, 
+		                       c.Name as Company_Name, r.Name as Recruiter_Name
+		                       FROM Job_Posting j
+		                       JOIN Company c ON j.Company_ID = c.Company_ID
+		                       JOIN Recruiter r ON j.Recruiter_Username = r.Username
+	    """
+
+		# Collect user selected field values
+		if location:
+			where_clauses.append("j.location LIKE :location")
+			params['location'] = "%" + location + "%"
+
+		if company:
+			where_clauses.append("c.Name LIKE :company")
+			params['company'] = "%" + company + "%"
+
+		if skills:
+			where_clauses.append("j.Skills LIKE :skills")
+			params['skills'] = "%" + skills + "%"
+
+		if experience:
+			where_clauses.append("j.Experience LIKE :experience")
+			params['experience'] = "%" + experience + "%"
+
+		if role_types:
+			# Depending on the role type selected, need to select jobs from the corresponding table
+			role_queries = []
+			if 'full_time' in role_types:
+				role_queries.append("j.Job_ID IN (SELECT Job_ID FROM Full_Time_Job)")
+			if 'internship' in role_types:
+				role_queries.append("j.Job_ID IN (SELECT Job_ID FROM Internship_Job)")
+			if 'coop' in role_types:
+				role_queries.append("j.Job_ID IN (SELECT Job_ID FROM Co_Op_Job)")
+			where_clauses.append("(" + " OR ".join(role_queries) + ")")
+
+		if search:
+			# If user is searching, set up filter query statements based on the keywords they entered
+			search_clauses = """j.location LIKE :search  
+							  OR c.Name LIKE :search
+							  OR j.Skills LIKE :search
+							  OR j.Job_Title LIKE :search
+							  OR j.Experience LIKE :search
+							  OR j.Requirements LIKE :search
+							  OR r.Name LIKE :search 
+		    """
+			where_clauses.append("(" + search_clauses + ")")
+			params["search"] = "%" + search + "%"
+
+		# Check which query to execute: query with user selected params if any, and if not any then default to show all
+		if where_clauses:
+			full_query = base_search_query + " WHERE " + " AND ".join(where_clauses)
 		else:
-			filter_query = """SELECT j.Job_ID,j.Job_Title, j.Experience, j.Location, j.Requirements, j.Skills, c.Name as Company_Name, r.Name as Recruiter_Name
-							FROM Job_Posting j
-							JOIN Company c ON j.Company_ID = c.Company_ID
-							JOIN Recruiter r ON j.Recruiter_Username = r.Username
-							WHERE j.location LIKE :search  
-							OR c.Name LIKE :search
-							OR j.Skills LIKE :search
-							OR j.Job_Title LIKE :search
-							OR j.Experience LIKE :search
-							OR j.Requirements LIKE :search
-							OR r.Name LIKE :search """
-			params = {}
-			params["search"] = "%"+search+"%"
-			# print(params)
-			filter_query = text(filter_query)
-			cursor = g.conn.execute(filter_query,params)
-			postings = cursor.fetchall()
-			# print(postings)
-			cursor.close()
-			return render_template('job_board.html',postings=postings)
+			full_query = base_search_query
+
+		# Execute query
+		cursor = g.conn.execute(text(full_query), params)
+		postings = cursor.fetchall()
+		cursor.close()
+
+		return render_template('job_board.html',postings=postings)
+
 	return render_template('job_board.html')
 
 @app.route('/reviews')
@@ -166,9 +193,9 @@ def reviews():
 							   FROM Review 
 							   WHERE Company_Feedback IS NOT NULL"""
 	interview_reviews = g.conn.execute(text(interview_reviews_query)).fetchall()
-	#print(interview_reviews)
+	print(interview_reviews)
 	company_reviews = g.conn.execute(text(company_reviews_query)).fetchall()
-	#print(company_reviews)
+	print(company_reviews)
 	return render_template("reviews.html", interview_reviews=interview_reviews, company_reviews=company_reviews)
 
 
